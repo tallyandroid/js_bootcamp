ae6e719b50a280a1a22014c81b5d1e51630889c9
Christian Tisby <cduane82@hotmail.com>
2020-01-30 01:22:24 -0500

    if statements

---
+++ app.js
@@ -0,0 +1,15 @@
+if ( 1 === 1) {
+ console.log("It's true")   
+}
+
+let rating = 3
+
+if (rating === 3) {
+    console.log('YOU ARE A SUPERSTAR')
+}
+
+let num = 37
+
+if (num % 2 !== 0) {
+    console.log('ODD NUMBER')
+}
