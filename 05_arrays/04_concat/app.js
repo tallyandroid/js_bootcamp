b3d5be522d9da01fbe222168112b445f06fd8387
Christian Tisby <cduane82@hotmail.com>
2020-02-01 12:24:23 -0500

    adding concat method

---
+++ 05_arrays/04_concat/app.js
@@ -0,0 +1,11 @@
+//used to merge two or more arrays
+
+const array1 = ['a', 'b', 'c']
+const array2 = ['d', 'e', 'f']
+
+console.log(array1.concat(array2))
+
+let fruits = ['apples', 'bananas']
+let veggies = ['broccoli', 'spinach']
+
+console.log(fruits.concat(veggies))
