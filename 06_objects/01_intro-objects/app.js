87feb77d54c05ee4616aaee1f96316d3a15e2a08
Christian Tisby <cduane82@hotmail.com>
2020-02-02 16:31:14 -0500

    adding accessing object properties

---
+++ 06_objects/01_intro-objects/app.js
@@ -0,0 +1,28 @@
+//totalSteps = 308727,
+//totalMiles = 211.7,
+//avgCalorieBurn = 5755,
+//workoutsThisWeek = '5 of 7',
+//avgGoodSleep = '2:13's
+
+
+
+
+const fitbitData = {
+    totalSteps: 308727,
+    totalMiles: 211.7,
+    avgCalorieBurn: 5744,
+    workoutsThisWeek: "5 of 7",
+    avgGoodSleep: "2:13" 
+}
+
+let data = {
+    a: 1,
+    b: 2,
+    c: 3
+}
+
+
+let steps = fitbitData.totalSteps
+console.log(steps)
+
+console.log(data)
---
+++ 06_objects/02_accessing-object-properties/app.js
@@ -0,0 +1,19 @@
+const numbers = {
+    100: 'one hundred',
+    16: 'sixteen'
+}
+
+//alternative way to access object properties
+console.log(numbers[100])
+
+const palette = {
+    red: '#eb4d4b',
+    yellow: '#f9ca24',
+    blue: '#30336b'
+}
+
+let mysteryColor = palette.red
+let otherMysteryColor = palette['yellow']
+console.log(mysteryColor)
+console.log(otherMysteryColor)
+
