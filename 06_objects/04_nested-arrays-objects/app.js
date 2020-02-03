ae91edf7690454e1e1dcdbc3919d162f732263bb
Christian Tisby <cduane82@hotmail.com>
2020-02-03 02:08:29 -0500

    adding nested-arrays-objects

---
+++ 06_objects/04_nested-arrays-objects/app.js
@@ -0,0 +1,31 @@
+const student = {
+    firstName: 'David',
+    lastName: 'Jones',
+    strengths: ['Music', 'Art'],
+    exams: {
+        midterm: 92,
+        final: 88
+    }
+}
+
+//This code will give the average
+const avg = (student.exams.midterm + student.exams.final) / 2
+
+const shoppingCart = [
+    {
+        product: 'Jenga Classic',
+        price: 6.88,
+        quantity: 1
+    },
+
+    {
+        product: 'Echo Dot',
+        price: 29.99,
+        quantity: 3
+    },
+    {
+        product: 'Fire Stick',
+        price: 39.99,
+        quantity: 2
+    }
+]
