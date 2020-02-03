dc92b6d10f80f24472dfea9bb92c20800d9d717d
Christian Tisby <cduane82@hotmail.com>
2020-02-03 02:26:55 -0500

    adding array-object-equality

---
+++ 06_objects/06_array_object-equality/app.js
@@ -0,0 +1,14 @@
+let nums = [1, 2, 3]
+let mystery = [1, 2, 3]
+
+
+const user = {
+    username: 'CherryGarcia8',
+    email: 'garcia@gmail.com',
+    notifications: []
+}
+
+if(!user.notifications.length) {
+    console.log("NO NEW NOTIFICATIONS!")
+}
+
