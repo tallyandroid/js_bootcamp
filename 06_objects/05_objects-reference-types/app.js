b99afe062d9f3fbdc184a1a9e56dc355c2e57dfc
Christian Tisby <cduane82@hotmail.com>
2020-02-03 02:17:31 -0500

    adding objects-reference-types git add . clear

---
+++ 06_objects/05_objects-reference-types/app.js
@@ -0,0 +1,10 @@
+const palette = {
+    red: '#eb4d4b',
+    yellow: '#f9ca24',
+    blue: '#30336b'
+}
+
+//these variables are pointing to the same object in memory
+const palette2 = palette
+
+palette2.green = '#ebf876'
