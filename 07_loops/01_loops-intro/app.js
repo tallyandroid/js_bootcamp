33b47b08c46823bafc1c9d510b283734c737ce73
Christian Tisby <cduane82@hotmail.com>
2020-02-04 00:49:55 -0500

    adding for loops

---
+++ 07_loops/01_loops-intro/app.js
@@ -0,0 +1,16 @@
+//For Loops
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log("hello:", i)
+// }
+
+
+// for (let num = 1; num <= 20; num++) {
+//     console.log(`${num}x${num} = ${num * num}`)
+// }
+
+
+for (let i = 200; i >= 0; i -= 25) {
+    console.log(i)
+}
+console.log('AFTER THE LOOP!')
