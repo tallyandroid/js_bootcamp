36f41e2297766edff2adc77f91bceec64601b2d9
Christian Tisby <cduane82@hotmail.com>
2020-02-08 00:50:03 -0500

    adding arguments

---
+++ 08_functions/03_arguments/app.js
@@ -0,0 +1,16 @@
+function greet(person) {
+    console.log(`Hi, ${person}!`)
+}
+
+function area(width, height) {
+    return width * height
+}
+
+console.log(area(2, 10))
+
+
+function rollDie() {
+    let roll = Math.floor(Math.random() * 6) + 1
+    console.log(`Rolled: ${roll}`)
+}
+
