82f31844cf035cfe15491c4362ffe7759dbe34b5
Christian Tisby <cduane82@hotmail.com>
2020-02-10 00:44:25 -0500

    adding more return statements

---
+++ 08_functions/06_more-returns/app.js
@@ -0,0 +1,25 @@
+// function square(x) {
+//     return x * x 
+//     console.log('ALL DONE')
+// }
+
+// function isPurple(color) {
+//     if(color.toLowerCase() === 'purple') {
+//         return true
+//     }  
+//     return false
+// }
+
+
+// function isPurple() {
+//     returncolor.toLowerCase() === 'purple'
+// }
+
+function containsPurple(arr) {
+    for(let color of arr) {
+        if(color === 'purple' || color === 'magenta') {
+            return true
+        }
+        return false
+    }
+}
