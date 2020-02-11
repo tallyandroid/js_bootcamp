b9d4c1770c1c232e16832c1de740acbbd0d9190c
Christian Tisby <cduane82@hotmail.com>
2020-02-10 22:46:34 -0500

    adding password validation

---
+++ 08_functions/07_challenge/app.js
@@ -0,0 +1,13 @@
+function isValidPassword(password, username) {
+    if (password.length < 8) {
+        return false
+    }
+    if(password.indexOf(' ') !== -1) {
+        return false
+    }
+    if(password.indexOf(username) !== -1) {
+        return false
+    }
+
+    return true
+}
