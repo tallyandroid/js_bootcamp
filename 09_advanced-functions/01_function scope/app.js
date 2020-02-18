a84b6f2d73b1cd2805b5eceb5203ba1ea6cc1da4
Christian Tisby <cduane82@hotmail.com>
2020-02-17 22:25:18 -0500

    adding function scope

---
+++ 09_advanced-functions/01_function scope/app.js
@@ -0,0 +1,21 @@
+function lol() {
+    let person = "Tom"
+    const age = 45
+    var color = "teal"
+    console.log(age)
+}
+
+function changeColor() {
+    let color = "purple"
+    const age = 19
+    console.log(age)
+}
+
+//each variable is scoped to each unique function
+
+
+let bird = "Mandarin Duck"
+
+function birdWatch() {
+    let bird = 'golden pheasant'
+}
