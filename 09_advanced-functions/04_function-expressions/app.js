b68825a4294ea9633ccc27f29fa24b4ee01329c5
Christian Tisby <cduane82@hotmail.com>
2020-02-21 22:31:06 -0500

    adding function expression

---
+++ 09_advanced-functions/04_function-expressions/app.js
@@ -0,0 +1,18 @@
+const square = function (num) {
+    return num * num
+}
+
+square(7)
+
+//anonymous function example
+
+function add(x, y) {
+    return x + y
+}
+
+//named function example
+const sum = function (x, y) {
+    return x + y
+}
+
+sum(9, 3)
