820d473100b472826d1af71f74adefc7dc581adf
Christian Tisby <cduane82@hotmail.com>
2020-03-04 21:48:20 -0500

    adding map function

---
+++ 10_array-callback-methods/02_map/app.js
@@ -0,0 +1,9 @@
+const texts = ['rofl', 'lol', 'omg', 'ttyl']
+const caps = texts.map(function (t) {
+    return t.toUpperCase()
+})
+
+
+const doubles = numbers.map(function (num) {
+    return num * 2
+}) 
