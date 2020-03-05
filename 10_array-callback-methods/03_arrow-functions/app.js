e8129e200c062e5e52dd5b792de41c98f52d9d2e
Christian Tisby <cduane82@hotmail.com>
2020-03-04 21:59:36 -0500

    adding arrow function example

---
+++ 10_array-callback-methods/03_arrow-functions/app.js
@@ -0,0 +1,21 @@
+//Arrow  Functions
+const square = (x) => {
+    return x * x
+}
+
+const sum = (x, y) => {
+    return x + y
+}
+
+// const square = function(x) {
+//     return x * x
+// }
+
+const isEven = (num) => {
+    return  num % 2 === 0
+}
+
+
+const multiply = (x, y) =>  {
+    return x * y
+}
