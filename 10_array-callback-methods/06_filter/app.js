627f9ac629e8dd1223002e26de3514c158348803
Christian Tisby <cduane82@hotmail.com>
2020-03-09 23:32:15 -0400

    adding filter method

---
+++ 10_array-callback-methods/06_filter/app.js
@@ -0,0 +1,14 @@
+// const nums = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+// const odds = nums.filter(n => {
+//     return n % 2 === 1
+// })
+
+// const smallNums = nums.filter(n => n < 5)
+
+const nums = [34, 35, 67, 54, 109, 102, 32, 9]
+
+const odds = nums.filter(n => n % 2 === 1)
+const evens = nums.filter(n => n % 2 === 0)
+
+const bigNums = nums.filter(n => n > 50)
+
