70fc05077a4d309d25f9e08d313b6e6839d23639
Christian Tisby <cduane82@hotmail.com>
2020-03-23 23:31:01 -0400

    adding spread function

---
+++ 11_miscellaneous-js/02_spread-function/app.js
@@ -0,0 +1,12 @@
+Math.max(3, 4, 5, 6, 7, 12, 13, 76, 99)
+
+function giveMeFour(a, b, c, d){
+    console.log('a', a)
+    console.log('b', b)
+    console.log('c', c)
+    console.log('d', d)
+}
+
+const colors = ['red', 'orange', 'yellow', 'green']
+
+//giveMeFour(...colors)
