09d2387105884fe2fe76cfa1b4701edcd926af82
Christian Tisby <cduane82@hotmail.com>
2020-03-24 23:55:14 -0400

    adding spread array example

---
+++ 11_miscellaneous-js/03_spread-array/app.js
@@ -0,0 +1,10 @@
+const cephalopods = ['dumbo octopus', 'humboldt squid', 'flamboyant cuttlefish']
+
+const gastropods = ['giant african snail', 'banana slug', 'variable neon slug']
+
+const cnidaria = ['fire coral', 'moon jelly']
+
+//spread array example
+const mollusca = [...cephalopods, ...gastropods]
+
+const inverts = [...cnidaria, ...gastropods, ...cephalopods]
